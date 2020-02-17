3bdfaf00f2a4af4b96e970db4948a5282b1e32aa
Jair Hinojosa <jhd011@g.ucla.edu>
2020-02-17 15:27:27 -0800

    Added dummy router element

--- client/src/components/Firebase/firebase.js
+++ client/src/components/Firebase/firebase.js
@@ -1,4 +1,6 @@
 import app from 'firebase/app';
+import 'firebase/auth';
+
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-import App from './components/App';
+import App from './components/App/App';
 import Firebase, {FirebaseContext} from './components/Firebase';
 
 ReactDOM.render(<App />, document.getElementById('root'));
