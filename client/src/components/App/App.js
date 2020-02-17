6cd0b15e97105eefc51b6746df8f7720c2f990ae
ktan17 <Kevin.Tan@gpiglobal.com>
2020-02-17 14:15:21 -0800

    added fonts

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import NavBar from '../NavBar/NavBar'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Typography } from '@material-ui/core';
 import './App.css';
+
+import NavBar from '../NavBar/NavBar';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#55A3FF' },
+    secondary: { main: '#FFEEB1' }
+  },
+  typography: {
+    fontFamily: 'Raleway, Arial'
+  }
+})
 
 function App() {
   return (
-    <div>
-      <header>
-        <NavBar/>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>
+        <header>
+          <NavBar/>
+        </header>
+        <Typography>i am gay</Typography>
+      </div>
+    </ThemeProvider>
   );
 }
 
