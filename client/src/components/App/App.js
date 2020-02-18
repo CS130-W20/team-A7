ebf960d5e2eef152fdf2680fa92c5ae27488eea6
Jair Hinojosa <jhd011@g.ucla.edu>
2020-02-17 16:34:29 -0800

    Added signup form, creates users in firebase

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import { Typography } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import './App.css';
 
-import Navigation from '../Navigation';
+import * as ROUTES from '../../constants/routes';
 
+import Navigation from '../Navigation';
 import NavBar from '../NavBar/NavBar';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
 
 const theme = createMuiTheme({
   palette: {
@@ -29,7 +40,16 @@ function App() {
         </header>
       </div>
       <Router>
-        <Navigation />
+        <div>
+          <hr />
+          <Route exact path={ROUTES.LANDING} component={LandingPage} />
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+          <Route path={ROUTES.HOME} component={HomePage} />
+          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
+        </div>
       </Router>
     </ThemeProvider>
   );
