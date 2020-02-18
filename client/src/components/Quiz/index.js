2a3b5279e9881d399e105f7e8a1db82a697a059f
Christina Oliveira <christinaoliveira@Christinas-MacBook-Pro.local>
2020-02-18 11:44:00 -0800

    added date pickers to quiz page

--- client/src/components/Quiz/index.js
+++ client/src/components/Quiz/index.js
@@ -1,7 +1,25 @@
-import React from 'react';
-const Quiz = () => (
-  <div>
-    <h1>Quiz</h1>
-  </div>
-);
-export default Quiz;
+import React, { useState } from 'react';
+import DateFnsUtils from '@date-io/date-fns'; 
+import { Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import {
+  DatePicker,
+  TimePicker,
+  DateTimePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
+
+
+
+function App() {
+  const [selectedDate, handleDateChange] = useState(new Date());
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <DatePicker value={selectedDate} onChange={handleDateChange} label="Departure Date" margin="normal" />
+      <DatePicker value={selectedDate} onChange={handleDateChange} label="Return Date" margin="normal" />
+    </MuiPickersUtilsProvider>
+    
+  );
+}
+export default App;
+ 
