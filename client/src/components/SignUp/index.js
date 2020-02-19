64d7f4727384a700e9de80acff5a891f8b37ca33
Kyle Romero <kyleromero98@gmail.com>
2020-02-18 21:17:35 -0800

    fixed sign in

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -33,9 +33,10 @@ class SignUpFormBase extends Component {
   };
   
   onSubmit = event => {
-    const { username, email, password } = this.state;
+    event.preventDefault();
+    const { username, email, passwordOne } = this.state;
     this.props.firebase
-      .doCreateUserWithEmailAndPassword(email, password)
+      .doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
         this.setState({ ...INITIAL_STATE });
         this.props.history.push(ROUTES.LANDING);
@@ -43,7 +44,6 @@ class SignUpFormBase extends Component {
       .catch(error => {
         this.setState({ error });
       });
-    event.preventDefault();
   };
 
   render() {
