f2d215011ce938607b3a19610e14ed76164deff5
Kyle Romero <kyleromero98@users.noreply.github.com>
2020-02-18 19:40:17 -0800

    fixed links (#42)

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -35,6 +35,7 @@ function App() {
         </header>
         <Switch>
             <Route exact path={ROUTES.LANDING} render={(props) => <Landing {...props} />} />
+            <Route exact path={ROUTES.ABOUT} render={(props) => <About {...props} />} />
             <Route exact path={ROUTES.SIGN_UP} render={(props) => <SignUp {...props} />} />
             <Route exact path={ROUTES.SIGN_IN} render={(props) => <SignIn {...props} />}/>
             <Route exact path={ROUTES.QUIZ} render={(props) => <Quiz {...props} />}/>
