6a116fc4d8eb083ab02698aee2e494ab83a05d72
Jair Hinojosa <jhd011@g.ucla.edu>
2020-03-07 14:26:51 -0800

    Added tabs and refactored booked/saved trips

--- client/src/components/MyTrips/index.js
+++ client/src/components/MyTrips/index.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
-import { withRouter, Link } from 'react-router-dom';
-import { compose } from 'recompose';
 
-import { withFirebase } from '../Firebase';
-import * as ROUTES from '../../constants/routes';
-
-import TripCard from './TripCard';
-import Trip, {Flight, HotelStay} from './Trip';
-
+import TripTabs from './TripTabs';
 import { airports } from "../TripGeneration/airport.js";
 
 const INITIAL_STATE = {
@@ -19,35 +12,17 @@ class MyTripsBase extends Component {
   constructor(props) {
     super(props);
     this.state = { ...INITIAL_STATE };
-    console.log("testing: ", airports[0].code);
-    // Creating trips
-    var departureDate = new Date(2020, 2, 24);
-    var returnDate = new Date(2020, 3, 24);
-    var departureFlight = new Flight(airports[0].city, airports[100].city, departureDate, airports[0], airports[100]);
-    var returnFlight = new Flight(airports[100].city, airports[0].city, returnDate, airports[100], airports[0]);
-    var hotelStay = new HotelStay('hotelId:1', departureDate, returnDate)
-    var trip = new Trip('Trip1', departureFlight, returnFlight, hotelStay);
-
-    // Creating list
-    this.state.trips.push(trip);
   }
 
   render() {
     return(
-      <div>
-        {(this.state.trips || []).map(item => (
-          <div key={0} style={{padding: 25}}>
-            <TripCard trip={item}/>
-          </div>
-        ))}
+      <div id="centered-masthead">
+        <TripTabs/>
       </div>
     );
   }
 }
 
-const MyTrips = compose(
-  withRouter,
-  withFirebase,
-)(MyTripsBase);
+const MyTrips = MyTripsBase;
 
 export default MyTrips;
