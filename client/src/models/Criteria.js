9b3c83122e7229beec0f9a1c4aff4005269b83d2
j-011 <56324501+j-011@users.noreply.github.com>
2020-03-09 01:55:37 -0700

    SavedTrip onClick (#85)
    * Added redirect when clicking SavedTrip
    
    * Clicking SavedTrip card now takes you to generate trip

--- client/src/models/Criteria.js
+++ client/src/models/Criteria.js
@@ -1,9 +1,10 @@
 class Criteria {
-  constructor(departureAirport, departureDate, returnDate, destination, budget) {
+  constructor(departureAirport, departureDate, returnDate, destination, price, budget) {
     this.departureAirport = departureAirport;
     this.departureDate = departureDate;
     this.returnDate = returnDate;
     this.destination = destination;
+    this.price = price;
     this.budget = budget;
   }
 }
