b73096e76721028f43fc2e7649dd23cc8a8eada4
Kyle Romero <kyleromero98@gmail.com>
2020-03-08 18:59:43 -0700

    added places folder

---
+++ client/src/components/Places/Places.js
@@ -0,0 +1,17 @@
+class Places {
+
+    // This class definition is wrong but this is the general idea
+  
+    // Given the lat/long of hotel returns a JSON object w/ nearby points of interest
+    json getPointsOfInterest (lat, long) {
+
+    }
+
+    // Given the lat/long returns image information 
+    img getImage (lat, long) {
+
+    }
+
+}
+
+export default Places;
