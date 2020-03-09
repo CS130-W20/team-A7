719922052c83a3b6e0d0af1efeed1562fe566b0f
Steven Lara <stevenplara@ucla.edu>
2020-03-08 22:38:54 -0700

    kyle my dying wish is with you

--- client/src/components/Places/Places.js
+++ client/src/components/Places/Places.js
@@ -1,17 +1,29 @@
+import React, { Component } from 'react';
+
 class Places {
+  // This class definition is wrong but this is the general idea
 
-    // This class definition is wrong but this is the general idea
-  
-    // Given the lat/long of hotel returns a JSON object w/ nearby points of interest
-    json getPointsOfInterest (lat, long) {
+  // Given the lat/long of hotel returns a JSON object w/ nearby points of interest
+  async getPointsOfInterest(latitude, longitude) {
+    var unirest = require("unirest");
+    var proxyurl = "https://cors-anywhere.herokuapp.com/";
+    return fetch(proxyurl + 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + latitude + ',' + longitude + '&radius=7500&type=tourist_attraction&key=AIzaSyC3Xpkfx9-L-4RHy87AP75GCoOUGSzpVS0');
+  }
 
-    }
+  // Given the lat/long returns image information 
+  getImage(lat, long) {
+    return;
+  }
 
-    // Given the lat/long returns image information 
-    img getImage (lat, long) {
-
-    }
-
-}
+};
 
 export default Places;
+
+/*
+var testPlace = new Places();
+testPlace.getPointsOfInterest(results[3].hotelResult.latitude, results[3].hotelResult.longitude).then(response => {
+  response.json().then(places => {
+    console.log(places.results);
+  });
+});
+*/
