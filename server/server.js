76a9839cab18b551b1876f8dfdc7e516938ecd53
ktan17 <Kevin.Tan@gpiglobal.com>
2020-03-10 21:17:30 -0700

    server stuff

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const app = require("express")();
 const { json } = require("body-parser");
 
+app.use(json());
+app.use("/generate-trip", require("./routes/generate-trip"));
+
 module.exports = {
   server: app
 };
