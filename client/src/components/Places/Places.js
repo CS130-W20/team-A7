e874ecb5c8a363b76050c116536cdb7371b8f7e6
Kyle Romero <kyleromero98@users.noreply.github.com>
2020-03-11 14:44:15 -0700

    Added attractions API frontend UI (#94)

--- client/src/components/Places/Places.js
+++ client/src/components/Places/Places.js
@@ -1,8 +1,34 @@
 
-// Returns a JSON object w/ nearby points of interest given a latitude and longitude
-export const getPointsOfInterest = async (latitude, longitude) => {
-    var proxyurl = "https://cors-anywhere.herokuapp.com/";
-    return fetch(proxyurl + 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + latitude + ',' + longitude + '&radius=7500&type=tourist_attraction&key=' + process.env.REACT_APP_PLACES_API_KEY); 
+// Returns a list of attractions data based on the city name
+export const getAttractions = async (cityName) => {
+  var proxyurl = "https://salty-gorge-09496.herokuapp.com/";
+  var apiKey = process.env.REACT_APP_PLACES_API_KEY;
+
+  return new Promise(function (resolve, reject) {
+    try {
+      fetch(proxyurl + 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=' + apiKey + '&input=' + cityName + '&inputtype=textquery').then((response) => {
+        // Finds the placeId based on the name of destination
+        return response.json()
+      }).then((response) => {
+        var placeId = response.candidates[0].place_id;
+        fetch(proxyurl + 'https://maps.googleapis.com/maps/api/place/details/json?key=' + apiKey + '&place_id=' + placeId).then((response) => {
+          // Uses the placeId to get more information on place
+          return response.json();
+        }).then((response) => {
+          var latitude = response.result.geometry.location.lat
+          var longitude = response.result.geometry.location.lng
+          fetch(proxyurl + 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + latitude + ',' + longitude + '&radius=7500&type=tourist_attraction&key=' + apiKey).then((response) => {
+            // Returns a list of attractions?
+            return response.json();
+          }).then((response) => {
+            resolve(response.results.slice(0, 3))
+          })
+        })
+      });  
+    } catch (e) {
+      reject(e)
+    }
+  });
 };
 
 // Returns a city image given the city name
