b97a05f5b07ad33c978a3a5da46d146c32971d46
Kevin Tan <Kevin.Tan@gpiglobal.com>
2020-03-13 22:29:37 -0700

    added docstrings (#101)
    * added docstrings
    
    * added docs

--- server/server.js
+++ server/server.js
@@ -1,6 +1,21 @@
+/**
+ * express module
+ * @const
+ */
 const app = require("express")();
+
+/**
+ * body-parser module for converting JSON request bodies to Javascript Objects.
+ */
 const { json } = require("body-parser");
 
+/**
+ * Middleware object that disables CORS requirements. Only to be used in development.
+ * 
+ * @param {Object} req The Request object.
+ * @param {Object} res The Response object.
+ * @param {function} next The next function in the middleware stack.
+ */
 const allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Headers', "*");
